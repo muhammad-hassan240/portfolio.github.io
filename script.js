2f11ef0bd078a95578e505fa722caabf1b5ea9be
muhammad-hassan240 <82748863+muhammad-hassan240@users.noreply.github.com>
2021-07-28 17:01:15 +0500

    portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+var scroll = new SmoothScroll('a[href*="#"]');
+
+var typed = new Typed('.type', {
+    strings: [
+        'Web Designer',
+        'Web Developer',
+        'Youtuber',
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
